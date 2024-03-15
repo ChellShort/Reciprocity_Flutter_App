a0aadeb5019aa17c04a2c5cea97aab74f948ede1
Ricardo <119143917+ChellShort@users.noreply.github.com>
2024-03-14 18:07:02 -0600

    Login que redirecciona

--- lib/ui/views/Pagina1.dart
+++ lib/ui/views/Pagina1.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class Pagina1 extends StatelessWidget {
@@ -7,7 +8,38 @@ class Pagina1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/ReciproCity.jpg'),
+                    SizedBox(height: 20),
+                    Text(
+                      'Oferta de tiempo limitado',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 20),
+                    Image.asset('assets/images/oferta.jpg'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: FractionalTranslation(
+              translation: Offset(0.0, -3.5),
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
